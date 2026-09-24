438c7262e06836c470a2441ec0ac1be5317c3e5a
Filippo Ferrari <filippoferrariofficial@gmail.com>
2026-09-24 11:16:02 +0000

    Lower internal hero crop slightly more

--- artifacts/filippo-ferrari/src/pages/Partner.tsx
+++ artifacts/filippo-ferrari/src/pages/Partner.tsx
@@ -87,7 +87,7 @@ export default function Partner() {
           className="absolute inset-0 bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(${HERO_BG})`,
-            backgroundPosition: "38% 62%",
+            backgroundPosition: "38% 66%",
           }}
         />
         <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/50 to-transparent" />
